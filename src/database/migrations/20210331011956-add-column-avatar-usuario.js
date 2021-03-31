c2553d9eaf03bd916553470c405bb9cd67d17dd2
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-03-30 22:32:18 -0300

    criada migracao table arquivos e relacao arquivo-usuario

---
+++ src/database/migrations/20210331011956-add-column-avatar-usuario.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('usuarios', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'arquivos', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.removeColumn('usuarios', 'avatar_id');
+  },
+};
