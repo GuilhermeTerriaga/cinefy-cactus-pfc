f9dc8eec361f02a8284838caf2f19a0ac1a1a567
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-04-30 22:10:28 -0300

    resolvido CRLF

--- src/database/migrations/20210331011956-add-column-avatar-usuario.js
+++ src/database/migrations/20210331011956-add-column-avatar-usuario.js
@@ -1,15 +1,11 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('usuarios', 'avatar_id', {
-      type: Sequelize.INTEGER,
-      references: { model: 'arquivos', key: 'id' },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL',
-      allowNull: true,
-    });
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.addColumn('usuarios', 'avatar_id', {
+    type: Sequelize.INTEGER,
+    references: { model: 'arquivos', key: 'id' },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+    allowNull: true,
+  }),
 
-  down: async (queryInterface) => {
-    return queryInterface.removeColumn('usuarios', 'avatar_id');
-  },
+  down: async (queryInterface) => queryInterface.removeColumn('usuarios', 'avatar_id'),
 };
