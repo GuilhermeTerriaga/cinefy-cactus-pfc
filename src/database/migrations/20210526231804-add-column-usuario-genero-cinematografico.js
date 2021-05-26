07fa3cb1be86f561682434f3d48e2c5f8b62b538
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-05-26 20:25:56 -0300

    migraçoes genero cinematografico, criadas

--- src/database/migrations/20210526231804-add-column-usuario-genero-cinematografico.js
+++ src/database/migrations/20210526231804-add-column-usuario-genero-cinematografico.js
@@ -1,21 +1,16 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     * Adicionar a tabela genero-cinematografico uma coluna "usuario_id"
      */
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
