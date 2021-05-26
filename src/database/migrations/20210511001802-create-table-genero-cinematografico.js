f0e55320a0925ab91214c4f813bf2c105d4e95a2
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-05-26 20:25:33 -0300

    migraçoes genero cinematografico, criadas

--- src/database/migrations/20210511001802-create-table-genero-cinematografico.js
+++ src/database/migrations/20210511001802-create-table-genero-cinematografico.js
@@ -7,31 +7,6 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
       },
-      veredito: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-      titulo: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      corpo: {
-        type: Sequelize.STRING(550),
-        allowNull: false,
-      },
-      nota: {
-        type: Sequelize.REAL,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
     });
   },
 
